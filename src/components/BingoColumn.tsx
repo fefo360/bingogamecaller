b99c733e7a7dc7df5ed276789e09a6e0edb9fea1
Rafael Ceballo <rafaelceballo@Rafaels-MacBook-Pro-2.local>
2022-06-01 00:07:57 -0400

    reduced board size on lg screen

--- src/components/BingoColumn.tsx
+++ src/components/BingoColumn.tsx
@@ -14,7 +14,7 @@ export default function BingoColumn({ letter, columnNumbers, range }: Props) {
     <div className="flex flex-col">
       <div
         id={letter}
-        className="flex items-center justify-center border bg-blue-800 text-xl lg:text-5xl font-extrabold text-white xl:h-14 md:w-44"
+        className="flex items-center justify-center border bg-blue-800 text-xl lg:text-5xl font-extrabold text-white xl:h-14 md:w-36"
       >
         {letter}
       </div>
@@ -25,7 +25,7 @@ export default function BingoColumn({ letter, columnNumbers, range }: Props) {
             number.isOnBoard
               ? "bg-blue-800 text-white "
               : "bg-blue-200 text-gray-400"
-          } h-6  w-20 flex-1 border border-white font-extrabold lg:text-5xl xl:w-44`}
+          } h-6  w-20 flex-1 border border-white font-extrabold lg:text-5xl xl:w-36`}
         >
           {number.number}
         </div>
